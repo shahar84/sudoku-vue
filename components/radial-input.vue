380a7b76b3fceed65458c2031fd4531be2d974a1
Shahar Polak <shaharp@breezometer.com>
2019-09-10 23:07:03 +0300

    Position the menu

--- components/radial-input.vue
+++ components/radial-input.vue
@@ -1,16 +1,19 @@
 <template>
-  <div>
-    <div style="margin-top: 40px">
-      <button @click="active=!active">Click</button>
-      {{active}}
+  <section>
+    <div class="radial-input-wrapper" :style="buttonWrapperStyle">
+      <button @click="active=!active" class="activation-button">
+        <slot>+</slot>
+      </button>
+      <ul class="circle-container" :style="parentStyle">
+        <li v-for="(item, index) in items" :key="item"
+            :style="itemStyle(index)" class="item">
+          <button @click="handleClick">
+            {{item}}
+          </button>
+        </li>
+      </ul>
     </div>
-
-    <ul class="circle-container2" :class="{'active':active}" :style="parentStyle">
-      <li v-for="(item, index) in items" :key="item" :style="itemStyle(index)">{{item}}</li>
-    </ul>
-
-
-  </div>
+  </section>
 </template>
 
 <script>
@@ -23,40 +26,87 @@ const directionOptions = Object.freeze({
   'FULL': 'FULL'
 })
 
-@Component({})
+@Component({
+  props: {
+    open: { type: Boolean, default: undefined }
+  }
+})
 export default class RadialInput extends Vue {
   active = false
+  isOpen = false
   circleSize = 300
   itemSize = 60
+  buttonSize = 60
   items = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   direction = directionOptions.FULL
+  manualMode
+
+  mounted () {
+    this.manualMode = typeof this.open !== 'undefined'
+    document.addEventListener('click', this.closeMenuEvent)
+  }
+
+  beforeDestroy () {
+    document.removeEventListener('click', this.closeMenuEvent)
+  }
+
+  get shouldOpen () {
+    const { open, manualMode, isOpen } = this
+    return manualMode ? open : isOpen
+  }
+
+  toggleMenu () {
+    if (this.manualMode) {
+      return
+    }
+    if (this.isOpen) {
+      this.isOpen = false
+      this.$emit('close')
+    } else {
+      this.isOpen = true
+      this.$emit('open')
+    }
+  }
+
+  handleClick () {
+    this.$emit('click')
+    this.toggleMenu()
+  }
 
   get angle () {
-    let angle;
+    let angle
     switch (this.direction) {
       case directionOptions.LEFT:
-        angle = -180;
-        break;
+        angle = -180
+        break
       case directionOptions.RIGHT:
-        angle = 180;
-        break;
+        angle = 180
+        break
       default:
-        angle = 360;
+        angle = 360
     }
     return angle / this.items.length
   }
 
-  get parentStyle () {
+  get buttonWrapperStyle () {
     return {
-      position: 'relative',
+      width: `${this.buttonSize}px`,
+      height: `${this.buttonSize}px`,
+    }
+  }
+
+  get parentStyle () {
+    const pointerEvents = this.active ? 'auto' : 'none'
+    return {
+      position: 'absolute',
       width: `${this.circleSize}px`,
       height: `${this.circleSize}px`,
       padding: 0,
       borderRadius: '50%',
       listStyle: 'none',
       boxSizing: 'content-box',
-      margin: '5em auto 0',
-      border: 'solid 5px tomato'
+      border: 'solid 5px tomato',
+      pointerEvents
     }
   }
 
@@ -73,6 +123,7 @@ export default class RadialInput extends Vue {
       `rotate(${rotate}deg)  translate(${translate}px) scale(1) rotate(${rotate * -1}deg)` :
       `rotate(-90deg) scale(0.8)`
     const transition = this.active ? '0.3s all' : '0.5s all'
+    const pointerEvents = this.active ? 'auto' : 'none'
 
     return {
       display: 'flex',
@@ -85,10 +136,11 @@ export default class RadialInput extends Vue {
       height: `${this.itemSize}px`,
       margin: `${margin}px`,
       background: 'gray',
-      borderRadius:'50%',
+      borderRadius: '50%',
       transform,
       opacity,
       transition,
+      pointerEvents
     }
   }
 
@@ -96,6 +148,24 @@ export default class RadialInput extends Vue {
 </script>
 
 <style scoped lang="scss">
+  .radial-input-wrapper {
+    position: relative;
+    background: #F5F5F5;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    button.activation-button {
+      background: #35495e;
+      width: 100%;
+      height: 100%;
+    }
+  }
 
+  .item /deep/ button {
+    position: relative;
+    display: block;
+    width: 100%;
+    height: 100%;
+  }
 
 </style>
